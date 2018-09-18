176c3e8c05e955a8a78aba2f8a15d75d038d693d
Colin <20szymbc@elmbrookstudents.org>
2018-09-18 08:45:19 -0500

    Movie

---
+++ src/outputExamples/Movie.java
@@ -0,0 +1,17 @@
+package outputExamples;
+
+public class Movie {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Arachnaphobia");
+		System.out.println(" CCC  RRRR   SSS");
+		System.out.println("C    R    R S");
+		System.out.println("C    R    R S");
+		System.out.println("C    RRRRR   SSS");
+		System.out.println("C    R R        S");
+		System.out.println("C    R  R       S");
+		System.out.println(" CCC R   R   SSS");
+	}
+
+}
