daa2d99bc1beed48ab42dba88d4a506081a44543
Colin <20szymbc@elmbrookstudents.org>
2018-09-21 08:58:58 -0500

    Fixing things

---
+++ src/outputExamples/Input.java
@@ -0,0 +1,15 @@
+package outputExamples;
+import java.util.Scanner;
+public class Input {
+	
+	public static void main(String [] args)
+	{
+		double height = -999;
+		Scanner input = new Scanner(System.in);
+		System.out.println("What is the height of a monkey?");
+		height = input.nextDouble();
+		System.out.println("The height of a monkey is " + height);
+	}
+	}
+
+
--- src/outputExamples/Movie.java
+++ src/outputExamples/Movie.java
@@ -5,13 +5,6 @@ public class Movie {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Arachnaphobia");
-		System.out.println(" CCC  RRRR   SSS");
-		System.out.println("C    R    R S");
-		System.out.println("C    R    R S");
-		System.out.println("C    RRRRR   SSS");
-		System.out.println("C    R R        S");
-		System.out.println("C    R  R       S");
-		System.out.println(" CCC R   R   SSS");
-	}
-
+		// This is unseen
+	}	
 }
