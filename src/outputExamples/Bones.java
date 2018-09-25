a40cf17b80e245c5d0beffdc1dff361044d95d11
Colin <20szymbc@elmbrookstudents.org>
2018-09-25 08:19:36 -0500

    Bones

---
+++ src/outputExamples/Bones.java
@@ -0,0 +1,13 @@
+package outputExamples;
+import java.util.Scanner;
+public class Bones {
+	
+	public static void main(String [] args)
+	{
+		int bones = 999;
+		Scanner input = new Scanner(System.in);
+		System.out.println("How many bones have you broken?");
+		bones = input.nextInt();
+		System.out.println("You have broken " + bones + " bones.");
+	}
+	}
